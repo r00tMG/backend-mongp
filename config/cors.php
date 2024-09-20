fe167b80d2c933511e9a4f7614a8c674634fbd56
r00tMG <meissagningue7@gmail.com>
2024-09-20 12:35:48 +0100

    add CD on repository

--- config/cors.php
+++ config/cors.php
@@ -20,7 +20,7 @@ return [
     'allowed_methods' => ['*'],
 
     #'allowed_origins' => [env('FRONTEND_URL', 'http://localhost:5173')],
-    'allowed_origins' => ['http://localhost:5173'],
+    'allowed_origins' => ['https://frontend-mongp.vercel.app/'],
 
     'allowed_origins_patterns' => [],
 

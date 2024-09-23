8f7d25c0b7b5d6059cb8e743de489f4a22ed7cd6
r00tMG <meissagningue7@gmail.com>
2024-09-23 20:50:52 +0100

    update UserController

--- app/Http/Controllers/Api/Admin/UserController.php
+++ app/Http/Controllers/Api/Admin/UserController.php
@@ -212,7 +212,7 @@ class UserController extends Controller
                 'message' => "Aucun utilisateur n'a été trouvé",
             ]);
         }
-        //User::where('ann',$id)->find($id)->delete();
+        User::find($id)->delete();
         return response()->json([
             'status' => Response::HTTP_NO_CONTENT,
             'message' => "Votre utilisateur a été supprimé",

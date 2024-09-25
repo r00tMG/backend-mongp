86f7be695fb051ec690cfa273562625fdb60a44b
r00tMG <meissagningue7@gmail.com>
2024-09-25 13:26:49 +0100

    add MessageResource

---
+++ app/Http/Resources/User/MessageController.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Http\Resources\User;
+
+use App\Http\Resources\Admin\UserResource;
+use Illuminate\Http\Request;
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class MessageController extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @return array<string, mixed>
+     */
+    public function toArray(Request $request): array
+    {
+        return [
+            'id' => $this->resource->id,
+            'contenu' => $this->resource->contenu,
+            'date_envoi' => $this->resource->date_envoi,
+            'emetteur' => new UserResource(
+                $this->resource->fromUser
+            ),
+            'recepteur' => new UserResource(
+                $this->resource->toUser
+            )
+        ];
+    }
+}
